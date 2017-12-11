35b47168a83d76fbe1f7a114648bc839364b26a0
Bacem <bacem-92@hotmail.fr>
2017-12-11 07:02:06 +0100

    -

---
+++ app/src/main/java/com/example/mohamed/openstarter/Adapters/TransformerAdapter.java
@@ -0,0 +1,45 @@
+package com.example.mohamed.openstarter.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.daimajia.slider.library.SliderLayout;
+import com.example.mohamed.openstarter.R;
+
+/**
+ * Created by Bacem on 12/11/2017.
+ */
+
+public class TransformerAdapter extends BaseAdapter{
+
+    private Context mContext;
+    public TransformerAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return SliderLayout.Transformer.values().length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return SliderLayout.Transformer.values()[position].toString();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView t = (TextView) LayoutInflater.from(mContext).inflate(R.layout.item_project_slider,null);
+        t.setText(getItem(position).toString());
+        return t;
+    }
+}
--- app/src/main/java/com/example/mohamed/openstarter/Fragments/TabProjectCampaignFragment.java
+++ app/src/main/java/com/example/mohamed/openstarter/Fragments/TabProjectCampaignFragment.java
@@ -6,25 +6,114 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
+import com.example.mohamed.openstarter.Adapters.TransformerAdapter;
 import com.example.mohamed.openstarter.R;
+
+import java.util.HashMap;
 
 /**
  * Created by Bacem on 11/27/2017.
  */
 
-public class TabProjectCampaignFragment extends Fragment {
+public class TabProjectCampaignFragment extends Fragment implements BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener{
 
     private static final String TAG = "TabProjectCampaignFragment";
+    private SliderLayout mDemoSlider;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_project_campaign_fragment,container,false) ;
 
+        initializeFragment(view);
+
         return  view ;
+
+    }
+
+    private void initializeFragment(View view) {
+
+        mDemoSlider = view.findViewById(R.id.slider);
+
+        HashMap<String,String> url_maps = new HashMap<String, String>();
+        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
+        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
+        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
+        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
+
+        for(String name : url_maps.keySet()){
+            TextSliderView textSliderView = new TextSliderView(getActivity());
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(url_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+
+            mDemoSlider.addSlider(textSliderView);
+        }
+
+        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
+        mDemoSlider.setDuration(4000);
+        mDemoSlider.addOnPageChangeListener(this);
+        ListView l = view.findViewById(R.id.transformers);
+        l.setAdapter(new TransformerAdapter(getActivity()));
+        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mDemoSlider.setPresetTransformer(((TextView) view).getText().toString());
+                Toast.makeText(getActivity(), ((TextView) view).getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public void onStop() {
+        // To prevent a memory leak on rotation, make sure to call stopAutoCycle() on the slider before activity or fragment is destroyed
+        mDemoSlider.stopAutoCycle();
+        super.onStop();
+    }
+
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        Toast.makeText(getActivity(),slider.getBundle().get("extra") + "",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
 
     }
 }
