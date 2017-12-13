7c22ef5377061614db1e5e2dd4732debbbd7a92c
Bacem <bacem-92@hotmail.fr>
2017-12-13 10:53:06 +0100

    Implementation of the image upload (still not integrated fully in the applicaiton)

---
+++ app/src/main/java/com/example/mohamed/openstarter/Data/DataSuppliers/MediaDs.java
@@ -0,0 +1,127 @@
+package com.example.mohamed.openstarter.Data.DataSuppliers;
+
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.mohamed.openstarter.Models.ProjectMedia;
+import com.example.mohamed.openstarter.app.AppController;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Bacem on 12/3/2017.
+ */
+
+public class MediaDs {
+
+    //**** URL STRINGS
+    private final String URL_SERVER = "http://192.168.1.3/androidws/web/app_dev.php";
+    //private final String URL_SERVER = "http://openstarter.000webhostapp.com/AndroidWS/web/app_dev.php";
+    private final String URL_GET_BY_PROJECT_PROJECT_MEDIA = URL_SERVER + "/media/project/get";
+    private final String URL_CREATE_PROJECT_MEDIA = URL_SERVER + "/media/project/create";
+
+    //**** TAG STRINGS
+    private final String TAG = "MEDIA-WS";
+    private final String REQUEST_TAG_GET_BY_PROJECT = "project_media_get_by_project_req";
+    private final String REQUEST_TAG_CREATE_PROJECT_MEDIA = "project_media_create_req";
+
+    private Gson mGson;
+
+    public MediaDs() {
+        GsonBuilder gsonBuilder =
+                new GsonBuilder()
+                        .setDateFormat("yyyy/MM/dd HH:mm:ss");
+        mGson = gsonBuilder.create();
+    }
+    //TODO:FIX
+    public void projectMediaGet(final String projectId, final MediaDs.Callback callback) {
+        JsonArrayRequest req = new JsonArrayRequest(Request.Method.POST,
+                URL_GET_BY_PROJECT_PROJECT_MEDIA,
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        ArrayList<ProjectMedia> mediasList = new ArrayList<>() ;
+                        mediasList.addAll(Arrays.asList(mGson.fromJson(response.toString(), ProjectMedia[].class)));
+                        callback.onSuccessProjectMediaGet(mediasList);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                String body = "";
+                //get status code here
+                //String statusCode = String.valueOf(error.networkResponse.statusCode);
+                //get response body and parse with appropriate encoding
+                if (error.networkResponse.data != null) {
+                    try {
+                        body = new String(error.networkResponse.data, "UTF-8");
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Log.e(TAG, body);
+
+            }
+        }) {
+            @Override
+            public byte[] getBody() {
+                String body = "{\"project_id\":" + projectId + "}";
+                return body.getBytes();
+            }
+        };
+
+
+        AppController.getInstance().addToRequestQueue(req, REQUEST_TAG_GET_BY_PROJECT);
+    }
+
+    public void projectMediaCreate(final String mediaName, final String isPrimary, final String mediaType, final String media, final String projectId, final MediaDs.Callback callback) {
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("media_name", mediaName);
+        params.put("is_primary", isPrimary);
+        params.put("media_type", mediaType);
+        params.put("media", media);
+        params.put("id_project", projectId);
+
+        Log.e("eeeee",params.toString() );
+
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.POST,
+                URL_CREATE_PROJECT_MEDIA,
+                new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        ProjectMedia createdProjectMedia = mGson.fromJson(response.toString(), ProjectMedia.class);
+                        callback.onSuccessProjectMediaCreate(createdProjectMedia);
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("eeeee","ezerer" );
+            }
+        }) {
+        };
+        AppController.getInstance().addToRequestQueue(jsonObjReq, REQUEST_TAG_CREATE_PROJECT_MEDIA);
+    }
+
+
+    public interface Callback {
+        void onSuccessProjectMediaGet(ArrayList<ProjectMedia> result);
+
+        void onSuccessProjectMediaCreate(ProjectMedia createdProjectMedia);
+    }
+}
--- app/src/main/java/com/example/mohamed/openstarter/Models/ProjectMedia.java
+++ app/src/main/java/com/example/mohamed/openstarter/Models/ProjectMedia.java
@@ -5,7 +5,6 @@ import org.greenrobot.greendao.annotation.Id;
 import org.greenrobot.greendao.annotation.Keep;
 import org.greenrobot.greendao.annotation.Property;
 import org.greenrobot.greendao.annotation.ToOne;
-import org.greenrobot.greendao.annotation.Transient;
 
 import java.util.Date;
 import org.greenrobot.greendao.annotation.Generated;
@@ -13,119 +12,118 @@ import org.greenrobot.greendao.DaoException;
 import org.greenrobot.greendao.annotation.NotNull;
 
 /**
- * Created by Bacem on 12/3/2017.
+ * Created by Bacem on 12/13/2017.
  */
 
 @Entity
-public class Comment {
+public class ProjectMedia {
 
-    //TODO: Put project
-    //****Persistent properties
     @Id(autoincrement = true)
     private long id;
     @Property
-    private String text;
+    private String name;
     @Property
-    private Date commentDate;
+    private String type;
+    @Property
+    private String path;
+    @Property
+    private Date uploadDate;
+    @Property
+    private boolean isPrimary;
     @Property
     @ToOne(joinProperty = "id")
-    private User user;
+    private Project project;
+
+
     /** Used to resolve relations */
     @Generated(hash = 2040040024)
     private transient DaoSession daoSession;
     /** Used for active entity operations. */
-    @Generated(hash = 1903578761)
-    private transient CommentDao myDao;
-    @Generated(hash = 251390918)
-    private transient Long user__resolvedKey;
-
-
-    public Comment() {
-    }
-
-    @Generated(hash = 97442839)
-    public Comment(long id, String text, Date commentDate) {
+    @Generated(hash = 265753341)
+    private transient ProjectMediaDao myDao;
+    @Generated(hash = 594172472)
+    public ProjectMedia(long id, String name, String type, String path,
+            Date uploadDate, boolean isPrimary) {
         this.id = id;
-        this.text = text;
-        this.commentDate = commentDate;
+        this.name = name;
+        this.type = type;
+        this.path = path;
+        this.uploadDate = uploadDate;
+        this.isPrimary = isPrimary;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Comment)) return false;
-
-        Comment comment = (Comment) o;
-
-        if (id != comment.id) return false;
-        return commentDate.equals(comment.commentDate);
+    @Generated(hash = 868025254)
+    public ProjectMedia() {
     }
-
-    @Override
-    public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + commentDate.hashCode();
-        return result;
-    }
-
     public long getId() {
-        return id;
+        return this.id;
     }
-
     public void setId(long id) {
         this.id = id;
     }
-
-    public String getText() {
-        return text;
+    public String getName() {
+        return this.name;
     }
-
-    public void setText(String text) {
-        this.text = text;
+    public void setName(String name) {
+        this.name = name;
     }
-
-    public Date getCommentDate() {
-        return commentDate;
+    public String getType() {
+        return this.type;
     }
-
-    public void setCommentDate(Date commentDate) {
-        this.commentDate = commentDate;
+    public void setType(String type) {
+        this.type = type;
     }
-
+    public String getPath() {
+        return this.path;
+    }
+    public void setPath(String path) {
+        this.path = path;
+    }
+    public Date getUploadDate() {
+        return this.uploadDate;
+    }
+    public void setUploadDate(Date uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+    public boolean getIsPrimary() {
+        return this.isPrimary;
+    }
+    public void setIsPrimary(boolean isPrimary) {
+        this.isPrimary = isPrimary;
+    }
+    @Generated(hash = 1005767482)
+    private transient Long project__resolvedKey;
     /** To-one relationship, resolved on first access. */
     @Keep
-    public User getUser() {
+    public Project getProject() {
         long __key = this.id;
-        if (user__resolvedKey == null || !user__resolvedKey.equals(__key)) {
+        if (project__resolvedKey == null || !project__resolvedKey.equals(__key)) {
             final DaoSession daoSession = this.daoSession;
             if (daoSession == null) {
-                if (this.user != null) return  this.user ;
+                if (this.project != null) return  this.project ;
                 throw new DaoException("Entity is detached from DAO context");
             }
-            UserDao targetDao = daoSession.getUserDao();
-            User userNew = targetDao.load(__key);
+            ProjectDao targetDao = daoSession.getProjectDao();
+            Project projectNew = targetDao.load(__key);
             synchronized (this) {
-                user = userNew;
-                user__resolvedKey = __key;
+                project = projectNew;
+                project__resolvedKey = __key;
             }
         }
-        return user;
+        return project;
     }
-
     /** called by internal mechanisms, do not call yourself. */
-    @Generated(hash = 1504227233)
-    public void setUser(@NotNull User user) {
-        if (user == null) {
+    @Generated(hash = 1673324817)
+    public void setProject(@NotNull Project project) {
+        if (project == null) {
             throw new DaoException(
                     "To-one property 'id' has not-null constraint; cannot set to-one to null");
         }
         synchronized (this) {
-            this.user = user;
-            id = user.getId();
-            user__resolvedKey = id;
+            this.project = project;
+            id = project.getId();
+            project__resolvedKey = id;
         }
     }
-
     /**
      * Convenient call for {@link org.greenrobot.greendao.AbstractDao#delete(Object)}.
      * Entity must attached to an entity context.
@@ -137,7 +135,6 @@ public class Comment {
         }
         myDao.delete(this);
     }
-
     /**
      * Convenient call for {@link org.greenrobot.greendao.AbstractDao#refresh(Object)}.
      * Entity must attached to an entity context.
@@ -149,7 +146,6 @@ public class Comment {
         }
         myDao.refresh(this);
     }
-
     /**
      * Convenient call for {@link org.greenrobot.greendao.AbstractDao#update(Object)}.
      * Entity must attached to an entity context.
@@ -161,12 +157,12 @@ public class Comment {
         }
         myDao.update(this);
     }
-
     /** called by internal mechanisms, do not call yourself. */
-    @Generated(hash = 2038262053)
+    @Generated(hash = 1188720252)
     public void __setDaoSession(DaoSession daoSession) {
         this.daoSession = daoSession;
-        myDao = daoSession != null ? daoSession.getCommentDao() : null;
+        myDao = daoSession != null ? daoSession.getProjectMediaDao() : null;
     }
-    
+
+
 }
